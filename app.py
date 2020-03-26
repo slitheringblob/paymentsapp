89a872cefb56f52298f07ecd6d3a93736597aeb9
Jaydeep Ganatra <jaydeepganatra11@gmail.com>
2020-03-26 19:21:50 +0530

    EOD Commit 26/03/2020
    Changed app.py to include compulsory user authentication and created new db file inside the base directory for ease of use. Added code to connect to the DB on login in app.py.

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask,render_template,redirect,url_for,request
+from flask import Flask,render_template,redirect,url_for,request,session,flash,g
+from functools import wraps
+import sqlite3
+
+
 app=Flask(__name__)
 
+app.secret_key= "my precious"
+app.database = "/db/vpt.db"
+
+################################login required decorator ####################################
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+############################################################################################# 
+##############################################    ROUTES    #################################
+
 @app.route('/')
+@login_required
 def hello():
-	return ' go to URL/login to access the software'
+	
+
+	g.db = connect_db()
+	return render_template("index.html")
+	
 
 @app.route('/home')
 def home():
@@ -16,9 +42,30 @@ def login():
 		if request.form['username']!='admin' or request.form['password']!='Pinkfloyd890!':
 			error = 'Invalid Credentials. Please try again.'
 		else:
+			session['logged_in'] = True
+			flash('You were just logged in!')
 			return redirect(url_for('home'))
 
 	return render_template('login.html',error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+	session.pop('logged_in',None)
+	flash('You were just logged Out!')
+	return redirect(url_for('login'))
+
+####################################### END ROUTES ##########################################################################
+
+def connect_db():
+    conn = None
+    try:
+        conn = sqlite3.connect(app.database)
+        print("Connection Established")
+        print(sqlite3.version)
+        return conn
+    except Error as e:
+        print(e)
 
 
 
--- testDB.py
+++ testDB.py
@@ -5,16 +5,13 @@ def create_Connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
+        print("Connection Established")
         print(sqlite3.version)
-
+        return conn
     except Error as e:
         print(e)
 
-    finally:
-        if conn:
-            conn.close()
-
 
 if __name__=='__main__':
-    create_Connection(r"C:\Users\jayde\Documents\sqlite3\db\paymentsappDB.db")
+    create_Connection(r"C:\Users\jayde\Documents\sqlite3\db\vpt.db")
     
