be97accd17cee982feac837f94d91e2ace041403
Jaydeep Ganatra <jaydeepganatra11@gmail.com>
2020-04-27 19:35:18 +0530

    EOD Commit 27/04/2020
    Added new Templates and form classes for the add_functionality. Updated the DB with some data and also updated main app routes to include new templates.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,redirect,url_for,request,session,flash,g
 from functools import wraps
 import sqlite3
+from forms import add_po_form,add_holiday_form,add_emp_form,add_fpn_form
 
 
 app=Flask(__name__)
@@ -19,21 +20,19 @@ def login_required(f):
             flash('You need to login first.')
             return redirect(url_for('login'))
     return wrap
+
 ############################################################################################# 
 ##############################################    ROUTES    #################################
 
 @app.route('/')
 @login_required
 def hello():
-	
+	pass
 
-	g.db = connect_db()
-	return render_template("index.html")
-	
-
-@app.route('/home')
-def home():
-	return render_template("home.html")
+@app.route('/dashboard')
+def dashboard():
+	#conn = connect_db()
+	return render_template("dashboard.html")
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -43,8 +42,8 @@ def login():
 			error = 'Invalid Credentials. Please try again.'
 		else:
 			session['logged_in'] = True
-			flash('You were just logged in!')
-			return redirect(url_for('home'))
+			# flash('You were just logged in!')
+			return redirect(url_for('dashboard'))
 
 	return render_template('login.html',error=error)
 
@@ -55,17 +54,46 @@ def logout():
 	flash('You were just logged Out!')
 	return redirect(url_for('login'))
 
+######################### Add_Functionality Routes ########################
+
+@app.route('/addnewpo')
+@login_required
+def addnewpo():
+	error = None
+	form = add_po_form()
+	return render_template("add_new_po.html",error=error,title = 'Add New PO',form = form)
+
+
+@app.route('/addnewholiday')
+@login_required
+def addnewholiday():
+	error = None
+	form = add_holiday_form()
+	return render_template("add_new_holiday.html",error=error,title = 'Add New PO',form = form)
+
+
+@app.route('/addnewfpn')
+@login_required
+def addnewfpn():
+	error = None
+	form = add_fpn_form()
+	return render_template("add_new_fpn.html",error=error,title = 'Add New PO',form = form)
+
+@app.route('/addnewemployee')
+@login_required
+def addnewemployee():
+	error = None
+	form = add_employee_form()
+	return render_template("add_new_employee.html",error=error,title = 'Add New PO',form = form)
+
+
 ####################################### END ROUTES ##########################################################################
 
 def connect_db():
-    conn = None
-    try:
-        conn = sqlite3.connect(app.database)
-        print("Connection Established")
-        print(sqlite3.version)
-        return conn
-    except Error as e:
-        print(e)
+	conn = sqlite3.connect(app.database)
+	print("Connection Established")
+	print(sqlite3.version)
+	return conn
 
 
 
---
+++ forms.py
@@ -0,0 +1,51 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField,TextAreaField,SubmitField,PasswordField,DateField,SelectField,IntegerField,DecimalField)
+from wtforms.validators import (DataRequired,Email,EqualTo,Length,URL)
+
+class add_po_form(FlaskForm):
+
+	resource_name = StringField('Resource Name',validators=[DataRequired()])
+	po_vendor = StringField('Vendor',validators = [DataRequired()])
+	month = StringField('Month', validators = [DataRequired()])
+	noofdays = IntegerField('No Of Days', validators = [DataRequired()])
+	leavestaken = DecimalField('Leaves Taken',validators = [DataRequired()])
+	billeddays = DecimalField('Billed Days',validators = [DataRequired()])
+	billingrate = DecimalField('Billing Rate',validators = [DataRequired()])
+	billableamount = DecimalField('Billable Amount', validators = [DataRequired()])
+	gst = DecimalField('GST' , validators = [DataRequired()])
+	total = DecimalField('Total',validators = [DataRequired()])
+	fpn = StringField('FPN',validators = [DataRequired()])
+	porf = StringField('PORF',validators = [DataRequired()])
+	project = StringField('Project',validators = [DataRequired()])
+	date_raised = DateField('Date Raised',format='%d-%m-%Y',validators = [DataRequired()])
+	pono = StringField('PO Number')
+	invoice_no = StringField('Invoice Number')
+	golive_date = DateField('Go live Date',format='%d-%m-%Y')
+	add_po = SubmitField('Add PO')
+
+class add_emp_form(FlaskForm):
+
+	emp_code = StringField('Employee Code',validators = [DataRequired()])
+	emp_name = StringField('Employee Name',validators = [DataRequired()])
+	vendor = SelectField('Vendor',validators = [DataRequired()],choices = [('Saksoft','saksoft'),('Clover','clover'),('MindCraft','mindcraft')])
+	joining_date  = DateField('Joining Date',format='%d-%m-%Y',validators = [DataRequired()])
+	rate = DecimalField('Rate',validators = [DataRequired()])
+	status = SelectField('Status',validators = [DataRequired()],choices = [('Active','active'),('Inactive','inactive')])
+	manager = StringField('Manager Emp Code',validators = [DataRequired()])
+	add_emp = SubmitField('Add Employee')
+
+class add_fpn_form(FlaskForm):
+	fpn = StringField('FPN',validators = [DataRequired()])
+	fpn_description = StringField('FPN Description',validators = [DataRequired()])
+	resource_duration = StringField('Resource Duration',validators = [DataRequired()])
+	amount = StringField('Amount',validators = [DataRequired()])
+	vendor = StringField('Vendors',validators = [DataRequired()])
+	opening_balance = DecimalField('Opening Balance',validators = [DataRequired()])
+	remaining_amount = DecimalField('Opening Balance',validators = [DataRequired()])
+	go_live = StringField('Go Libe Date')
+	remarks = StringField('Remarks', validators = [DataRequired()])
+
+class add_holiday_form(FlaskForm):
+
+	date = DateField('Holiday Date',format='%d-%m-%Y',validators = [DataRequired()])
+	reason = StringField('Holiday Reason',validators = [DataRequired()])
