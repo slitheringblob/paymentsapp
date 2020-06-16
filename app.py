6f13bf9054d4b4ebf2fba9b785867a09baacff90
Jaydeep Ganatra <jaydeepganatra11@gmail.com>
2020-06-16 20:49:45 +0530

    EOD Commit 16/06/20
    Multiple application level Changes. Entirely new View and Update Functionality.

--- app.py
+++ app.py
@@ -29,7 +29,9 @@ def login_required(f):
 @app.route('/')
 @login_required
 def hello():
-	pass
+
+	error = "Invalid route. Go to /login"
+	return render_template('login.html',error=error)
 
 @app.route('/dashboard',methods=['GET','POST'])
 @login_required
@@ -88,7 +90,7 @@ def addnewpo():
 		invoice_no = form.invoice_no.data
 		golive_date = form.golive_date.data
 		
-		c.execute("INSERT INTO MS_PO_MASTER VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(resource_name,po_vendor,month,noofdays,leavestaken,billeddays,billingrate,billableamount,gst,total,fpn,porf,project,date_raised,pono,invoice_no,golive_date,))
+		c.execute("INSERT INTO MS_PO_MASTER(resource_name,po_vendor,month,noofdays,leavestaken,billeddays,billingrate,billableamount,gst,total,fpn,porf,project,date_raised,pono,invoice_no,golive_date) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(resource_name,po_vendor,month,noofdays,leavestaken,billeddays,billingrate,billableamount,gst,total,fpn,porf,project,date_raised,pono,invoice_no,golive_date,))
 		conn.commit()
 		conn.close()
 
@@ -116,7 +118,7 @@ def addnewholiday():
 	conn = connect_db()
 	if form.validate_on_submit():
 		c = conn.cursor()
-		c.execute("INSERT INTO MS_HOLIDAY_MASTER VALUES(?,?)",(date,reason,))
+		c.execute("INSERT INTO MS_HOLIDAY_MASTER(date,reason) VALUES(?,?)",(date,reason,))
 
 		flash(f'Added New Holiday for {reason} on {date} !', 'success')
 		print('validated holiday')
@@ -137,10 +139,32 @@ def addnewholiday():
 def addnewfpn():
 	error = None
 	form = add_fpn_form()
+	conn = connect_db()
 	if form.validate_on_submit():
-		flash(f'Added new FPN for {form.fpn.data} and {form.opening_balance.data}!', 'success')
-		return redirect(url_for('dashboard')) 
+		c = conn.cursor()
 
+		fpn = form.fpn.data
+		fpn_description = form.fpn_description.data
+		resource_duration = form.resource_duration.data
+		amount = form.amount.data
+		vendor = form.vendor.data
+		opening_balance = form.opening_balance.data
+		amount_utilized = form.amount_utilized.data
+		remaining_amount = form.remaining_amount.data
+		go_live = form.go_live.data
+		remarks = form.remarks.data
+		
+
+		c.execute("INSERT INTO MS_FPN_MASTER(fpn,fpn_description,resource_duration,amount,vendor,opening_balance,amount_utilized,remaining_amount,go_live,remarks) VALUES(?,?,?,?,?,?,?,?,?,?)",(fpn,fpn_description,resource_duration,amount,vendor,opening_balance,amount_utilized,remaining_amount,go_live,remarks,))
+		conn.commit()
+		conn.close()
+
+		print('validated form')
+		flash(f'Added FPN:{fpn}!', 'success')
+		return redirect(url_for('dashboard'))
+	else:
+		print(form.errors)
+		print('invalid form')
 
 	return render_template("add_new_fpn.html",error=error,title = 'Add New FPN',form = form)
 
@@ -149,8 +173,26 @@ def addnewfpn():
 def addnewemployee():
 	error = None
 	form = add_employee_form()
-
+	conn = connect_db()
+	
 	if form.validate_on_submit():
+
+		c = conn.cursor()
+		emp_code = form.emp_code.data
+		emp_name = form.emp_name.data
+		vendor = form.vendor.data
+		joining_date  = form.joining_date.data
+		rate = form.rate.data
+		status = form.status.data
+		manager = form.manager.data
+
+		# print("DATA TYPE OF JOINING DATE:",type(joining_date))
+		# print("DATA TYPE OF RATE:",type(rate))
+		
+		c.execute("INSERT INTO MS_EMP_MASTER(emp_code,emp_name,vendor,joining_date,rate,status,manager) VALUES(?,?,?,?,?,?,?)",(emp_code,emp_name,vendor,joining_date,rate,status,manager,))
+		conn.commit()
+		conn.close()
+		
 		flash(f'Added Employee {form.emp_code.data}!', 'success')
 		return redirect(url_for('dashboard'))
 
@@ -162,60 +204,76 @@ def addnewemployee():
 @login_required
 def viewpo():
 	error = None
-	form = view_po_form()
+	# form = view_po_form()
 
-	pono = form.view_po_pono.data
-	porf = form.view_po_porf.data
-	fpn = form.view_po_fpn.data
-	invoice_no = form.view_po_invoice_no.data
+	# pono = form.view_po_pono.data
+	# porf = form.view_po_porf.data
+	# fpn = form.view_po_fpn.data
+	# invoice_no = form.view_po_invoice_no.data
 
-	conn = connect_db()
-	c = conn.cursor()
+	# conn = connect_db()
+	# c = conn.cursor()
 
-	if form.validate_on_submit():
+	# if form.validate_on_submit():
 
-		if pono=="" and porf=="" and fpn=="" and invoice_no=="":
-			return render_template("view_po.html",error = error,title = 'View PO',form=form)
+	# 	if pono=="" and porf=="" and fpn=="" and invoice_no=="":
+	# 		return render_template("view_po.html",error = error,title = 'View PO',form=form)
 		
-		if pono=="" and porf=="" and fpn=="":
-			c.execute("select * from MS_PO_MASTER WHERE invoice_no=?",(invoice_no,))
-			rows = c.fetchall()
-			return render_template("view_po_results.html",error = error,title = 'Search Results',form=form,rows = rows)
+	# 	if pono=="" and porf=="" and fpn=="":
+	# 		c.execute("select * from MS_PO_MASTER WHERE invoice_no=?",(invoice_no,))
+	# 		rows = c.fetchall()
+	# 		return render_template("view_po_results.html",error = error,title = 'Search Results',form=form,rows = rows)
 
-		if pono=="" and porf=="" and invoice_no=="":
-			c.execute("select * from MS_PO_MASTER WHERE fpn=?",(fpn,))
-			rows = c.fetchall()
-			return render_template("view_po_results.html",error = error,title = 'Search Results',form=form,rows = rows)
+	# 	if pono=="" and porf=="" and invoice_no=="":
+	# 		c.execute("select * from MS_PO_MASTER WHERE fpn=?",(fpn,))
+	# 		rows = c.fetchall()
+	# 		return render_template("view_po_results.html",error = error,title = 'Search Results',form=form,rows = rows)
 
-		if pono=="" and invoice_no=="" and fpn=="":
-			c.execute("select * from MS_PO_MASTER WHERE porf=?",(porf,))
-			rows = c.fetchall()
-			return render_template("view_po_results.html",error = error,title = 'Search Results',form=form,rows = rows)
+	# 	if pono=="" and invoice_no=="" and fpn=="":
+	# 		c.execute("select * from MS_PO_MASTER WHERE porf=?",(porf,))
+	# 		rows = c.fetchall()
+	# 		return render_template("view_po_results.html",error = error,title = 'Search Results',form=form,rows = rows)
 
-		if invoice_no=="" and porf=="" and fpn=="":
-			c.execute("select * from MS_PO_MASTER WHERE pono=?",(pono,))
-			rows = c.fetchall()
-			return render_template("view_po_results.html",error = error,title = 'Search Results',form=form,rows = rows)
+	# 	if invoice_no=="" and porf=="" and fpn=="":
+	# 		c.execute("select * from MS_PO_MASTER WHERE pono=?",(pono,))
+	# 		rows = c.fetchall()
+	# 		return render_template("view_po_results.html",error = error,title = 'Search Results',form=form,rows = rows)
+	if error==None:
+		conn = connect_db()
+		c = conn.cursor()
+		c.execute("select * from MS_PO_MASTER")
+		rows = c.fetchall()
 
+		return render_template("view_po_results.html",error = error,title = "All Raised Purchase Orders",rows = rows)
 
+	
 	return render_template("view_po.html",error = error,title = 'View PO',form=form)
 
 @app.route('/viewfpn',methods = ['GET','POST'])
 @login_required
 def viewfpn():
+	# error = None
+	# form = view_fpn_form()
+
+	# conn = connect_db()
+	# c = conn.cursor()
+
+	# if form.validate_on_submit():
+	# 	fpn = form.view_fpn_fpn.data
+
+	# 	c.execute("select * from MS_FPN_MASTER WHERE fpn=?",(fpn,))
+	# 	rows = c.fetchall()
+
+	# 	return render_template("view_fpn_results.html",error = error,title = 'Search Results',form=form,rows = rows)
 	error = None
-	form = view_fpn_form()
 
-	conn = connect_db()
-	c = conn.cursor()
-
-	if form.validate_on_submit():
-		fpn = form.view_fpn_fpn.data
-
-		c.execute("select * from MS_FPN_MASTER WHERE fpn=?",(fpn,))
+	if error==None:
+		conn = connect_db()
+		c = conn.cursor()
+		c.execute("select * from MS_FPN_MASTER")
 		rows = c.fetchall()
 
-		return render_template("view_fpn_results.html",error = error,title = 'Search Results',form=form,rows = rows)
+		return render_template("view_fpn_results.html",error = error,title = '',rows = rows)
 
 
 
@@ -250,10 +308,10 @@ def viewholiday():
 @login_required
 
 def updatepo():
-	error = None
-	form = update_po_search_form()
 
-	return render_template("update_po_search.html",error = error,title = "Find PO",form = form)
+	return render_template("update_po_search.html", title="Update PO")
+
+
 
 @app.route('/updatefpn',methods=['GET','POST'])
 @login_required
@@ -269,9 +327,67 @@ def updateholiday():
 
 @app.route('/updateemployee',methods=['GET','POST'])
 @login_required
-
 def updateemployee():
-	pass
+	
+	if request.method == "POST":
+		emp_code = request.form["emp_code"]
+		emp_name = request.form["emp_name"]
+		vendor = request.form["vendor"]
+		joining_date  = request.form["joining_date"]
+		rate = request.form["rate"]
+		status = request.form["status"]
+		manager = request.form["manager"]
+
+		conn = connect_db()
+		c = conn.cursor()
+
+		c.execute("UPDATE MS_EMP_MASTER SET emp_code=?,emp_name=?,vendor=?,joining_date=?,rate=?,status=?,manager=? WHERE emp_code=?",(emp_code,emp_name,vendor,joining_date,rate,status,manager,emp_code,))
+
+		conn.commit()
+		conn.close()
+
+		return render_template("update_test.html",title = "VPT- Update")
+
+
+
+
+
+
+
+@app.route('/updateemptest',methods = ['GET','POST'])
+@login_required
+def updateemptest():
+	
+	if request.method == "POST":
+
+		conn = connect_db()
+		c = conn.cursor()
+		emp_code = request.form["emp_code"]
+		emp_name = request.form["emp_name"]
+		vendor = request.form["vendor"]
+		joining_date  = request.form["joining_date"]
+		rate = request.form["rate"]
+		status = request.form["status"]
+		manager = request.form["manager"]
+
+		c.execute("INSERT INTO MS_EMP_MASTER(emp_code,emp_name,vendor,joining_date,rate,status,manager) VALUES(?,?,?,?,?,?,?)",(emp_code,emp_name,vendor,joining_date,rate,status,manager,))
+		conn.commit()
+		conn.close()
+
+	return render_template("update_test.html",title = "Update")
+
+@app.route('/updatetest',methods = ['GET','POST'])
+@login_required
+def updatetest():
+	conn = connect_db()
+	c = conn.cursor()
+	c.execute("select * from MS_EMP_MASTER")
+	rows = c.fetchall()
+
+	return render_template("update_test.html", rows=rows, title="VPT")
+
+
+
 
 
 
